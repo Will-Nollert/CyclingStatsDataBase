f1b5d32920b7bc2b41d952922f539ec8422d210d
William Nollert <will.nollert@gmail.com>
2023-04-18 12:56:27 -0500

    feat: scraping logic for indivudal rider metaData

--- Server/public/index.js
+++ Server/public/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const scrapeRaceResults = (raceName, raceYear) => {
+/* const scrapeRaceResults = (raceName, raceYear) => {
   const raceUrl = `https://www.procyclingstats.com/race/${raceName}/${raceYear}/results`;
 
 axios
@@ -82,4 +82,59 @@ axios
 };
 
 // Call the function with the initial raceName and raceYear
-scrapeRaceResults("e3-harelbeke", "2023");
+scrapeRaceResults("e3-harelbeke", "2023"); */
+
+const url = 'https://www.procyclingstats.com/rider/dmitriy-gruzdev';
+
+axios.get(url).then((response) => {
+  const html = response.data;
+  const $ = cheerio.load(html);
+const riderRelativeStrengthInfo = $('.left.w50.mb_w100');
+// Nationality
+const nationality = riderRelativeStrengthInfo.find('b:contains("Nationality:")').next().next().text().trim();
+// GC
+const gcRawScore = $('div.bg.red').attr('style').match(/width:\s*(\d+(\.\d+)?)%/)[1];
+//round to two decimal places
+const gcRoundScore = parseFloat(gcRawScore).toFixed(2);
+// Time Trial
+const timeTrialRawScore = $('div.bg.blue').attr('style').match(/width:\s*(\d+(\.\d+)?)%/)[1];
+const timeTrialRoundScore = parseFloat(timeTrialRawScore).toFixed(2);
+// Sprint
+const sprintRawScore = $('div.bg.orange').attr('style').match(/width:\s*(\d+(\.\d+)?)%/)[1];
+const sprintRoundScore = parseFloat(sprintRawScore).toFixed(2);
+// Climber
+const climberRawScore = $('div.bg.purple').attr('style').match(/width:\s*(\d+(\.\d+)?)%/)[1];
+const climberRoundScore = parseFloat(climberRawScore).toFixed(2);
+
+const riderInfo = $('.left.w50.mb_w100').text().trim();
+const dateOfBirthRegex = /Date of birth:\s*([\d]+(?:th|st|nd|rd)\s+\w+\s+\d{4})/;
+const weightRegex = /Weight:\s*([\d.]+)\s*kg/;
+const heightRegex = /Height:\s*([\d.]+)\s*m/;
+const ageRegex = /\((\d+)\)/;
+
+const dateOfBirthMatch = riderInfo.match(dateOfBirthRegex);
+const weightMatch = riderInfo.match(weightRegex);
+const heightMatch = riderInfo.match(heightRegex);
+const ageMatch = riderInfo.match(ageRegex);
+
+const dateOfBirth = dateOfBirthMatch ? dateOfBirthMatch[1] : null;
+const weight = weightMatch ? weightMatch[1] : null;
+const height = heightMatch ? heightMatch[1] : null;
+const age = ageMatch ? ageMatch[1] : null;
+
+
+
+// Log data fields
+console.log('Date of birth:', dateOfBirth);
+console.log('Nationality:', nationality);
+console.log('Age:', age);
+console.log('Weight:', weight, "kg");
+console.log('Height:', height, "m");
+console.log("GC:", gcRoundScore);
+console.log("Time Trial:",  timeTrialRoundScore);
+console.log("Sprint:",  sprintRoundScore);
+console.log("Climber:", climberRoundScore);
+  
+}).catch((error) => {
+  console.error(error);
+});
