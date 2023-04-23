e86798ec0f225cd22d321edf5b6f7e49c0725872
William Nollert <will.nollert@gmail.com>
2023-04-22 20:57:56 -0500

    feat: Race-Modle update for stage and recursize function call logic

--- Server/public/index.js
+++ Server/public/index.js
@@ -2,72 +2,78 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const scrapeRaceResults = (raceName, raceYear, stage) => {
-  const raceUrl = `https://www.procyclingstats.com/race/tour-de-france/2022/stage-18`;
-  axios
-    .get(raceUrl)
-    .then((response) => {
-      const $ = cheerio.load(response.data);
-      //Get the Race MetaData
-      const infoList = $("ul.infolist");
-      //create an object to hold the race data and add the raceFinishers array
-      let race = {};
-      //get the race date from the infolist
-      infoList.find("li").each((index, element) => {
-        const key = $(element)
-          .find("div")
-          .eq(0)
-          .text()
-          .trim()
-          .replace(/[\s.:]+/g, "_")
-          .toLowerCase();
-        const value = $(element)
-          .find("div")
-          .eq(1)
-          .text()
-          .trim()
-          .replace(/[\s.]+/g, "_");
-        race[key] = value;
-      });
-      //Add a key value pair to the race object of raceName
-      race = { name: raceName, stage: stage, ...race };
-      //Get the race finishers  data
-      const table = $("table.results > tbody");
-      //create an array to hold the finishers
-      const finishers = [];
-      //Scrape Data from the table
-      table.find("tr").each((index, element) => {
-        const position = $(element).find("td").eq(0).text().trim();
-        //get Rider Name for 1 day classics
-        /* const riderName = $(element)
+  const raceUrl = `https://www.procyclingstats.com/race/${raceName}/${raceYear}/stage-${stage}`;
+  axios.get(raceUrl).then((response) => {
+    const $ = cheerio.load(response.data);
+    //Get the Race MetaData
+    const infoList = $("ul.infolist");
+    //create an object to hold the race data and add the raceFinishers array
+    let race = {};
+    //get the race date from the infolist
+    infoList.find("li").each((index, element) => {
+      const key = $(element)
+        .find("div")
+        .eq(0)
+        .text()
+        .trim()
+        .replace(/[\s.:]+/g, "_")
+        .toLowerCase();
+      const value = $(element)
+        .find("div")
+        .eq(1)
+        .text()
+        .trim()
+        .replace(/[\s.]+/g, "_");
+      race[key] = value;
+    });
+    //Add a key value pair to the race object of raceName
+    race = { name: raceName, stage_: stage, ...race };
+    //Get the race finishers  data
+    const table = $("table.results > tbody");
+    //create an array to hold the finishers
+    const finishers = [];
+    //Scrape Data from the table
+    table.find("tr").each((index, element) => {
+      const position = $(element).find("td").eq(0).text().trim();
+      //get Rider Name for 1 day classics
+      /* const riderName = $(element)
           .find("td")
           .eq(3)
           .find("a")
           .first()
           .text()
           .trim(); */
-          //get Rider Name for Grand Tours
-          const riderName = $(element).find('a[href^="rider/"]').text().trim();
-          const teamName = $(element).find('a[href^="team/"]').text().trim();
-        /* const teamName = $(element)
+      //get Rider Name for Grand Tours
+      const riderName = $(element).find('a[href^="rider/"]').text().trim();
+      const teamName = $(element).find('a[href^="team/"]').text().trim();
+      /* const teamName = $(element)
           .find("td")
           .eq(3)
           .find(".showIfMobile")
           .text()
           .trim(); */
-        const nameParts = riderName.split(" ");
-        const lastName = nameParts
-          .pop()
-          .replace(/\s/g, "")
-          .normalize("NFD")
-          .replace(/[\u0300-\u036f]/g, "");
-        const formattedName = `${lastName.toLowerCase()}-${nameParts
-          .map((part) => part.toLowerCase())
-          .join("-")}`;
-          //Add the finisher to the finishers array
-        finishers.push({ position, riderName: formattedName, teamName });
-      });
-      console.log(race, finishers)
-      // Make the POST request to your backend API endpoint
+      const nameParts = riderName.split(" ");
+      const lastName = nameParts
+        .pop()
+        .replace(/\s/g, "")
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+      const formattedName = `${lastName.toLowerCase()}-${nameParts
+        .map((part) => part.toLowerCase())
+        .join("-")}`;
+      //Add the finisher to the finishers array
+      finishers.push({ position, riderName: formattedName, teamName });
+    });
+    console.log(`Added ${raceName} ${raceYear} stage-${stage} to the database`);
+    if (stage >= 2) {
+      // Call the function recursively with a lower raceYear
+      scrapeRaceResults(raceName, raceYear, stage - 1);
+    }
+    if (stage === 1 && raceYear > 1950) {
+      // Call the function recursively with a lower raceYear
+      scrapeRaceResults(raceName, raceYear - 1, 21);
+    }
+    // Make the POST request to your backend API endpoint
     /*   fetch("http://localhost:3000/api/races/races-with-finishers", {
         method: "POST",
         headers: {
@@ -92,11 +98,12 @@ const scrapeRaceResults = (raceName, raceYear, stage) => {
             scrapeRaceResults(raceName, raceYear - 1, stage -1);
           }
         }) */
-    })
+  });
 };
 
 //define an array of races names
-const oneDayClassicRaces = ["e3-harelbeke",
+const oneDayClassicRaces = [
+  "e3-harelbeke",
   "paris-roubaix",
   "strade-bianche-donne",
   "milano-sanremo",
@@ -116,12 +123,9 @@ const oneDayClassicRaces = ["e3-harelbeke",
   "milano-torino",
 ];
 
-const grandTours = ["tour-de-france"];
+const grandTours = ["tour-de-france", "giro-d-italia", "vuelta-a-espana"];
 
 // Call the function with the initial raceName and raceYear
-/* grandTours.forEach((raceName) => {
+grandTours.forEach((raceName) => {
   scrapeRaceResults(raceName, 2022, 21);
 });
- */
-
-scrapeRaceResults();
